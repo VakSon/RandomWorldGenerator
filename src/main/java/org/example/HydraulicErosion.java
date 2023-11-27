c8ade8c48037335dc7db71ad06001c0a807935f2
VakSon <67511157+VakSon@users.noreply.github.com>
2023-11-27 19:05:47 +0100

    HydraulicErosion getting me desperate

--- src/main/java/org/example/HydraulicErosion.java
+++ src/main/java/org/example/HydraulicErosion.java
@@ -9,28 +9,27 @@ public class HydraulicErosion {
     int sizeX;
     int sizeY;
     public float[][] heightMap;
-    int dropsNum = 100000;
+    int dropsNum = 1000;
     int maxLifeTime = 30;
     float evaporateSpeed = 0.01f;
-    double water = 1f;
-    double minSlope = 0.01f;
-    double minimalWaterVolume = 0.05;
-    int radius = 3;
-    double inertia = 0.1;
-    double erosionSpeed = 0.3f;
-    double depositionSpeed = 0.3f;
-    double minSedimentCapacity = 0.01f;
-    double minSpeed = 0.15;
-    double sedimentCapacityFactor = 2;
+    float water = 1f;
+    float minimalWaterVolume = 0.05f;
+    int radius = 2;
+    float inertia = 0.1f;
+    float erosionSpeed = 0.3f;
+    float depositionSpeed = 0.3f;
+    float minSedimentCapacity = 0.01f;
+    float minSpeed = 0.05f;
+    float sedimentCapacityFactor = 2;
 
     public HydraulicErosion(float[][] heightMap){
         sizeX = heightMap.length;
         sizeY = heightMap[0].length;
         this.heightMap = heightMap;
-        erode();
+        run();
     }
-    public void erode(){
-        double totalMeshBefore = 0;
+    public void run(){
+        float totalMeshBefore = 0;
         for (int i = 0; i < sizeX; i++){
             for (int j = 0; j < sizeY; j++){
                 totalMeshBefore += heightMap[i][j];
@@ -38,10 +37,10 @@ public class HydraulicErosion {
         }
         Random r = new Random(Settings.seed);
         for(int drops = 0; drops < dropsNum;drops++){
-            droplet(r.nextDouble(sizeX),r.nextDouble(sizeY));
+            droplet(r.nextFloat(sizeX),r.nextFloat(sizeY));
             //System.out.println();
         }
-        double totalMeshAfter = 0;
+        float totalMeshAfter = 0;
         for (int i = 0; i < sizeX; i++){
             for (int j = 0; j < sizeY; j++){
                 totalMeshAfter += heightMap[i][j];
@@ -49,25 +48,25 @@ public class HydraulicErosion {
         }
         System.out.println(totalMeshBefore + "     " + totalMeshAfter);
     }
-    public void droplet(double x ,double y){
+    public void droplet(float x ,float y){
         //represents how many tiles it will go for one lifetime cyclus
-        double speed = 1f;
-        double height = evaluateHeight(x,y);
-        double[] direction = new double[2];
-        double dwater = water;
-        double dsediment = 0f;
+        float speed = 1f;
+        float height = evaluateHeight(x,y);
+        float[] direction = new float[2];
+        float dwater = water;
+        float dsediment = 0f;
 
 
         for (int lifetime = 0; lifetime<maxLifeTime;lifetime++){
 
             //create new direction vector
-            double[] newDirection = evaluateDirection(x,y);
+            float[] newDirection = evaluateDirection(x,y);
 
             direction[0] = direction[0]*inertia - newDirection[0]*( 1 - inertia);
             direction[1] = direction[1]*inertia - newDirection[1]*( 1 - inertia);
 
             //normalise the vector (to have length of one)
-            double lengthOfDirection =Math.sqrt(direction[0]*direction[0] + direction[1]*direction[1]);
+            float lengthOfDirection = (float)Math.sqrt(direction[0]*direction[0] + direction[1]*direction[1]);
 
             if(lengthOfDirection != 0) {
                 direction[0] = direction[0] / lengthOfDirection;
@@ -83,17 +82,17 @@ public class HydraulicErosion {
 
             //count the erosion or sedimentation amount
 
-            double newHeight = evaluateHeight(x,y);
-            double diffHeight = newHeight-height;
+            float newHeight = evaluateHeight(x,y);
+            float diffHeight = newHeight-height;
 
             //calculate maximal sediment capacity in new pos
-            double sedimentCapacity = Math.max(minSedimentCapacity, -diffHeight * speed * dwater * sedimentCapacityFactor);
+            float sedimentCapacity = Math.max(minSedimentCapacity, -diffHeight * speed * dwater * sedimentCapacityFactor);
 
             //if moving uphill or carrying more than it should
             if (dsediment > sedimentCapacity || diffHeight > 0){
 
 
-                double amountToDeposit = (diffHeight > 0) ? Math.min(diffHeight, dsediment) : (dsediment - sedimentCapacity) * depositionSpeed;
+                float amountToDeposit = (diffHeight > 0) ? Math.min(diffHeight, dsediment) : (dsediment - sedimentCapacity) * depositionSpeed;
                 dsediment -= amountToDeposit;
 
                 //add sediment to four nodes using bilinear interpolation (again :((( )
@@ -105,24 +104,24 @@ public class HydraulicErosion {
             else {
                 //erode part of ground around droplet
                 //calculate amount to erode cannot be higher than diff in height => create pit holes
-                double amountToErode = Math.min((sedimentCapacity - dsediment) * erosionSpeed,-diffHeight);
+                float amountToErode = Math.min((sedimentCapacity - dsediment) * erosionSpeed,-diffHeight);
 
                 //for all nodes in radius from NW node
-                List<Double> weights = new ArrayList<>();
+                List<Float> weights = new ArrayList<>();
 
                 List<Integer> indexes = new ArrayList<>();
 
-                double weightSum = 0;
+                float weightSum = 0;
 
                 int index = 0;
                 for (int i = -radius; i <= radius; i++) {
                     for (int j = -radius; j <= radius; j++) {
                         //distance to x,y
-                        double distance = Math.sqrt(i*i + j*j);
+                        float distance = (float) Math.sqrt(i*i + j*j);
                         //if its in range
                         if (Math.sqrt(i*i + j*j) <= radius){
                             //weight
-                            double weight = 1 - distance/radius;
+                            float weight = 1 - distance/radius;
                             if(x+i > 0 && x+i < sizeX && y+j > 0 && y+j < sizeY) {
                                 weightSum += weight;
                                 indexes.add(i + (int)(x) + (((int)(y) + j)  * sizeX));
@@ -148,60 +147,56 @@ public class HydraulicErosion {
             }
 
             //update speed and amount of water
-            speed = Math.sqrt(speed*speed - diffHeight * 0.01);
+            speed = (float) Math.sqrt(speed*speed - diffHeight * 4);
             if (isNaN(speed)) {
                 speed = minSpeed;
             }
-
+            dwater = (1-evaporateSpeed);
         }
-
-            //System.out.println(speed);
-            dwater *= (1-evaporateSpeed);
-
     }
-    double[] evaluateDirection(double x,double y){
+    float[] evaluateDirection(float x,float y){
 
-        double[] variables = offsetsAndNodeHeights(x,y);
+        float[] variables = offsetsAndNodeHeights(x,y);
 
-        double offsetX = variables[0];
-        double offsetY = variables[1];
+        float offsetX = variables[0];
+        float offsetY = variables[1];
 
         //height at node points
-        double tl = variables[2];
-        double tr = variables[3];
-        double dl = variables[4];
-        double dr = variables[5];
+        float tl = variables[2];
+        float tr = variables[3];
+        float dl = variables[4];
+        float dr = variables[5];
 
-        double gradientX = (tr - tl) * (1 - offsetY) + (dr - dl) * offsetY;
-        double gradientY = (dl - tl) * (1 - offsetX) + (dr - tl) * offsetX;
+        float gradientX = (tr - tl) * (1 - offsetY) + (dr - dl) * offsetY;
+        float gradientY = (dl - tl) * (1 - offsetX) + (dr - tl) * offsetX;
 
 
         //System.out.println("\nHeightNW " + tl +" HeightNE " + tr + "\nX " + x + " Y " + y + "\nHeightSW " + dl +" HeightSE " + dr + "\nGradient X " + gradientX + " GradientY " + gradientY + "\n");
 
-        return new double[]{gradientX,gradientY};
+        return new float[]{gradientX,gradientY};
     }
-    double evaluateHeight(double x,double y){
+    float evaluateHeight(float x,float y){
 
-        double[] variables = offsetsAndNodeHeights(x,y);
+        float[] variables = offsetsAndNodeHeights(x,y);
 
-        double offsetX = variables[0];
-        double offsetY = variables[1];
+        float offsetX = variables[0];
+        float offsetY = variables[1];
 
         //height at node points
-        double tl = variables[2];
-        double tr = variables[3];
-        double dl = variables[4];
-        double dr = variables[5];
+        float tl = variables[2];
+        float tr = variables[3];
+        float dl = variables[4];
+        float dr = variables[5];
 
         // Calculate height with bilinear interpolation of the heights of the nodes of the cell
         return (tl * (1 - offsetX) * (1 - offsetY) + tr * offsetX * (1 - offsetY) + dl * (1 - offsetX) * offsetY + dr * offsetX * offsetY);
     }
-    double[] offsetsAndNodeHeights(double x,double y){
-        double tl,tr,dl,dr;
+    float[] offsetsAndNodeHeights(float x,float y){
+        float tl,tr,dl,dr;
 
 
-        double offsetX = x - (int)x;
-        double offsetY = y - (int)y;
+        float offsetX = x - (int)x;
+        float offsetY = y - (int)y;
 
         //height at node points
         tl = heightMap[(int)wrap(x,sizeX)][(int)(wrap(y,sizeY))];
@@ -209,9 +204,9 @@ public class HydraulicErosion {
         dl = heightMap[(int)wrap(x,sizeX)][(int)wrap(y+1,sizeY)];
         dr = heightMap[(int)wrap(x+1,sizeX)][(int)wrap(y+1,sizeY)];
 
-        return new double[]{offsetY, offsetY,tl,tr,dl,dr};
+        return new float[]{offsetY, offsetY,tl,tr,dl,dr};
     }
-    double wrap(double i, int max){
+    float wrap(float i, int max){
         return i%max;
     }
 
